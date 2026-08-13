212ebab812d5f9d2a591d5a230104f8873f657a5
Dusan Ilic <dusan.ilic@factoryww.com>
2026-08-13 09:49:03 +0200

    fix: Toast need to be in the Center

--- src/lib/toast.ts
+++ src/lib/toast.ts
@@ -1,15 +1,15 @@
 import { notification } from "antd";
 
 const success = (msg: string, desc?: string) =>
-  notification.success({ message: msg, description: desc });
+  notification.success({ message: msg, description: desc, placement: "bottomRight" });
 
 const error = (msg: string, desc?: string) =>
-  notification.error({ message: msg, description: desc });
+  notification.error({ message: msg, description: desc, placement: "bottomRight"  });
 
 const warning = (msg: string, desc?: string) =>
-  notification.warning({ message: msg, description: desc });
+  notification.warning({ message: msg, description: desc, placement: "bottomRight"  });
 
 const info = (msg: string, desc?: string) =>
-  notification.info({ message: msg, description: desc });
+  notification.info({ message: msg, description: desc, placement: "bottomRight"  });
 
 export const toast = { success, error, warning, info };
